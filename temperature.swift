cbf5703d02337717eef027fa38ae7f355163e8a6
Evge4ait <156699711+Evge4ait@users.noreply.github.com>
2025-03-27 21:18:40 +0600

    Update temperature.swift

--- temperature.swift
+++ temperature.swift
@@ -1,28 +1,39 @@
-let city = "Omsk"
-let temperature = 20
+let userName = "User"
+let city = "Omsk" // научиться выводить текст из массива
+let temperature = -40
 
-switch city {
-case "Omsk":
-    print("your location is Omsk")
-case "Moscow":
-    print("your location is Moscow")
-case "Saint Petersburg":
-    print( "your location is Saint Petersburg")
-default:
-    print("your location is not in our database")
-}
 
-if temperature > 25 {
+
+let name = userName
+let welcomeText = "Welcome, \(name)!"
+print(welcomeText)
+
+var massiveCity: [String] = ["unknown location","Omsk", "Moscow", "Saint Petersburg", "Myrmansk", "Kazan", "Samara", "Nizhny Novgorod", "Volgograd", "Yekaterinburg", "Chelyabinsk", "Tomsk", "Novosibirsk", "Krasnodar", "Ulyanovsk", "Yaroslavl", "Kogasayevsk", "Astrakhan", "Rostov-on-Don", "Samara"]
+
+print (massiveCity[0])
+
+
+
+let tVeryHot = 35
+let tHot = 25
+let tWarm = 15
+let tNormal = 0
+let tCold = -10
+let tVeryCold = -20
+let tVeryVeryCold = -30
+
+let t = temperature
+
+if t > tHot && t < tVeryHot {
     print ("it is very hot today \(temperature)°C")
-} else if temperature > 15 {
+} else if t > tWarm && t < tHot {
     print ("it is warm today \(temperature)°C")
-} else if temperature > 0 {
+} else if t > tNormal && t < tWarm {
     print ("it is cold today \(temperature)°C")
-} else if temperature > -10 {
+} else if t > tCold && t < tNormal {
     print( "it is very cold today \(temperature)°C")
-} else if temperature > -20 {
+} else if t > tVeryCold && t < tVeryVeryCold {
     print( "it is very very cold today \(temperature)°C")
-} else {
-    print( "it is freezing today \(temperature)°C")
+} else if t > tVeryHot || t < tVeryVeryCold {
+    print( "Warring abnormal temperature \(temperature)°C")
 }
-
